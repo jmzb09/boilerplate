e9700ec71a965e65011bbd62dc3fe54fa1a010b1
jmzb09 <ilust98@gmail.com>
2020-04-16 19:37:06 +0300

    Third commit

--- webpack.config.dev.babel.js
+++ webpack.config.dev.babel.js
@@ -46,8 +46,7 @@ module.exports = {
         test: /\.(sa|sc|c)ss$/,
         use: [
           {
-            loader: 'style-loader',
-            options: { sourceMap: true },
+            loader: 'style-loader'
           },
           {
             loader: 'css-loader',
